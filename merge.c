576791c4f348e897463ec72bd0272bde221389bd
v-pranavve <v-pranavve@microsoft.com>
2025-08-18 00:13:30 +0530

    final changes

--- merge.c
+++ merge.c
@@ -2,12 +2,14 @@
     Implement a function that merges two sorted arrays into a third array using pointers
 */
 
+// Online C compiler to run C program online
 #include <stdio.h>
 
-void merge(int *c, int* d, int size_a, int size_b)
+void sort_merge(int *c, int* d, int size_a, int size_b)
 {
     int size = size_a + size_b;
     int e[size];
+    int temp;
     for(int i=0;i<size;i++)
     {
         if(i<size_a)
@@ -17,17 +19,29 @@ void merge(int *c, int* d, int size_a, int size_b)
     }
     for(int j=0;j<size;j++)
     {
-        printf("%d, ",e[j]);
+        for(int k=j+1;k<size;k++)
+        {
+            if(e[j]>e[k])
+            {
+                temp=e[j];
+                e[j]=e[k];
+                e[k]=temp;
+            }
+        }
+    }
+    for(int l=0;l<size;l++)
+    {
+        printf("%d, ",e[l]);
     }
 }
 
 int main() {
-    int a[5] = {1,2,3,4,5};
-    int b[5] = {6,7,8,9,0};
+    int a[5] = {0,9,5,7,3};
+    int b[5] = {1,2,4,6,8};
     int *c = a;
     int *d = b;
     int size_a = sizeof(a)/sizeof(int);
     int size_b = sizeof(b)/sizeof(int);
-    merge(c,d,size_a,size_b);
+    sort_merge(c,d,size_a,size_b);
     return 0;
 }
