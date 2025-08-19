433a9cc9aebd18403ed1e7027529aeae90653be3
v-pranavve <v-pranavve@microsoft.com>
2025-08-20 01:15:25 +0530

    added the script

--- Lowercase.c
+++ Lowercase.c
@@ -6,14 +6,31 @@
 
 void lowercase(char *a)
 {
-    printf("The entered uppercase string: %s",a);
-    
+    printf("The entered string: %s \n", a);
+    char b[20]; // Target buffer
+    char *d = b;
+
+    while (*a)
+    {
+        if (*a >= 'A' && *a <= 'Z')
+            *d = *a + 32; // Convert to lowercase
+        else
+            *d = *a; // Leave as is
+
+        a++;
+        d++;
+    }
+
+    *d = '\0'; // Null-terminate the string
+
+    printf("Corresponding lowercase string: %s \n", b);
 }
+
 int main()
 {
     char a[20];
-    printf("Enter the uppercase string: \n");
-    scanf("%s",a);
-    lpwercase(a);
+    printf("Enter the string (uppercase or mixed): \n");
+    scanf("%s", a);
+    lowercase(a);
     return 0;
 }
